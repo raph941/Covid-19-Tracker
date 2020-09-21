82c2ef669b0a442eb2965ef4d7ff4d77ca4e397c
raph941 <raph941@gmail.com>
2020-09-21 08:23:26 +0100

    chore: redployment to netlify setup

---
+++ build/precache-manifest.bb1f303cf9bfb45f55ad5f34a9ef837a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba57a6c107d05549f4df6781a1f3e2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3085b789945274199397",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "689729c949e7025e9259",
+    "url": "/static/css/main.0d1b1948.chunk.css"
+  },
+  {
+    "revision": "3085b789945274199397",
+    "url": "/static/js/2.ad4750e4.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.ad4750e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "689729c949e7025e9259",
+    "url": "/static/js/main.e67d5477.chunk.js"
+  },
+  {
+    "revision": "8a73b3fa4209a4f8a0c9",
+    "url": "/static/js/runtime-main.396c5088.js"
+  }
+]);
